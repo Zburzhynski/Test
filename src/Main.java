0244e7c04f1fd68c13901084dd665c02781f938e
Uladzimir Zburzhynski <uladzimir_zburzhynski@epam.com>
2012-03-07 16:03:37 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -232,7 +232,7 @@ public class Main {
         List<Manager> m = new ArrayList<Manager>();
 //
         GenericUtil.copyList(m, m);
-		System.out.println("Hello world");
+		System.out.println("Test of work");
 
     }
 
